ca55c3822c00e64c45987e19f3c6b345238486e7
amarpreet-jangral1 <amarpreetjangral4@gmail.com>
2025-05-16 20:01:09 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
-// mongoose.connect("mongodb://localhost:27017/myproject")
-mongoose.connect("mongodb+srv://amar:02052025@cluster0.khvl6rj.mongodb.net/myproject")
+mongoose.connect("mongodb://localhost:27017/myproject")
+// mongoose.connect("mongodb+srv://amar:02052025@cluster0.khvl6rj.mongodb.net/myproject")
 
     .then(() => {
         console.log("database connected");
